decab54970c154d20907c61ccff468d96ab5ed85
leha03333 <alekseivisnak@gmail.com>
2023-05-14 15:49:43 +0700

    dobavil vivodi

---
+++ hello/hello/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hi hi hi");
+            Console.ReadLine();
+        }
+    }
+}
