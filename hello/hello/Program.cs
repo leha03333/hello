3a39bf367167450af2ccd82f1598011ec6949a07
leha03333 <alekseivisnak@gmail.com>
2023-05-14 16:05:31 +0700

    dobavil 2 vvoda

--- hello/hello/Program.cs
+++ hello/hello/Program.cs
@@ -6,6 +6,8 @@
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hi hi hi");
+            Console.WriteLine("izmenil");
+            Console.WriteLine("vivodi");
             Console.ReadLine();
         }
     }
